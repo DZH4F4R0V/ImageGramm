8e7b5d2013b65ec2681258ba6a1ee97fde9ffc6a
Jeff <jeff-122@yandex.ru>
2026-04-08 13:40:18 +0300

    Initial Commit

---
+++ ImageGrammTests/ImageGrammTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGrammTests.swift
+//  ImageGrammTests
+//
+//  Created by J_Eff on 08.04.2026.
+//
+
+import Testing
+@testable import ImageGramm
+
+struct ImageGrammTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
