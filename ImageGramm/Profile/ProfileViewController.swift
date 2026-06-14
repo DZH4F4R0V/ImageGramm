5eb19de2b0d1023b8d6b0b9f445fef655adae4ed
Dzhafar <jeff-122@ya.ru>
2026-06-15 01:52:40 +0300

    fixed review comments

--- ImageGramm/Profile/ProfileViewController.swift
+++ ImageGramm/Profile/ProfileViewController.swift
@@ -9,63 +9,85 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    private var avatarView: UIImageView!
+    private var logoutButton: UIButton!
+    private var fioLabel: UILabel!
+    private var emailLabel: UILabel!
+    private var aboutLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
+        setupConstraints()
+        configureAppearance()
+    }
+    
+    private func setupUI() {
         let avatarImage = UIImage(named: "photo")
-        let avatarView = UIImageView()
+        avatarView = UIImageView()
         avatarView.image = avatarImage
         avatarView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(avatarView)
+        
+        guard let buttonImage = UIImage(named: "exit") else {
+            print("Button image not found!")
+            return
+        }
+        logoutButton = UIButton.systemButton(
+            with: buttonImage,
+            target: self,
+            action: #selector(didTapLogoutButton)
+        )
+        logoutButton.tintColor = UIColor(named: "YP Red (iOS)")
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoutButton)
+        
+        fioLabel = UILabel()
+        fioLabel.text = "Екатерина Новикова"
+        fioLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(fioLabel)
+        
+        emailLabel = UILabel()
+        emailLabel.text = "@ekaterina_nov"
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(emailLabel)
+        
+        aboutLabel = UILabel()
+        aboutLabel.text = "Hello, world!"
+        aboutLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(aboutLabel)
+    }
+    
+    private func setupConstraints() {
         avatarView.heightAnchor.constraint(equalToConstant: 70).isActive = true
         avatarView.widthAnchor.constraint(equalTo: avatarView.heightAnchor).isActive = true
         avatarView.topAnchor.constraint(equalTo: view.topAnchor, constant: 52).isActive = true
         avatarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
         
-        guard let buttonImage = UIImage(named: "exit") else {
-            fatalError("buttonIcon not found")
-        }
-        let logoutButton = UIButton.systemButton(
-            with: buttonImage,
-            target: self,
-            action: #selector(Self.didTapLogoutButton)
-        )
-        logoutButton.tintColor = UIColor(named: "YP Red (iOS)")
-        logoutButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(logoutButton)
         logoutButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
         logoutButton.widthAnchor.constraint(equalTo: logoutButton.heightAnchor).isActive = true
         logoutButton.centerYAnchor.constraint(equalTo: avatarView.centerYAnchor).isActive = true
         logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
-        
-        let fioLabel = UILabel()
-        fioLabel.text = "Екатерина Новикова"
-        fioLabel.font = UIFont(name: "YSDisplay-Bold", size: 23)
-        fioLabel.textColor = UIColor(named: "YP White (iOS)")
-        fioLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(fioLabel)
+
         fioLabel.topAnchor.constraint(equalTo: avatarView.bottomAnchor, constant: 8).isActive = true
         fioLabel.leadingAnchor.constraint(equalTo: avatarView.leadingAnchor).isActive = true
-        
-        
-        let emailLabel = UILabel()
-        emailLabel.text = "@ekaterina_nov"
-        emailLabel.font = UIFont(name: "YSDisplay-Medium", size: 13)
-        emailLabel.textColor = UIColor(named: "YP Gray (iOS)")
-        emailLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(emailLabel)
+
         emailLabel.topAnchor.constraint(equalTo: fioLabel.bottomAnchor, constant: 8).isActive = true
         emailLabel.leadingAnchor.constraint(equalTo: avatarView.leadingAnchor).isActive = true
-        
-        let aboutLabel = UILabel()
-        aboutLabel.text = "Hello, world!"
-        aboutLabel.font = UIFont(name: "YSDisplay-Medium", size: 13)
-        aboutLabel.textColor = UIColor(named: "YP White (iOS)")
-        aboutLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(aboutLabel)
+
         aboutLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 8).isActive = true
         aboutLabel.leadingAnchor.constraint(equalTo: avatarView.leadingAnchor).isActive = true
-        
+    }
+    
+    private func configureAppearance() {
+        fioLabel.font = UIFont(name: "YSDisplay-Bold", size: 23)
+        fioLabel.textColor = UIColor(named: "YP White (iOS)")
+            
+        emailLabel.font = UIFont(name: "YSDisplay-Medium", size: 13)
+        emailLabel.textColor = UIColor(named: "YP Gray (iOS)")
+            
+        aboutLabel.font = UIFont(name: "YSDisplay-Medium", size: 13)
+        aboutLabel.textColor = UIColor(named: "YP White (iOS)")
     }
     
     @objc
